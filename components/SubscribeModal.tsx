db5f856389614e371db3b4a5caa642b656dcf6a7
Fellipe Marcel <fmrclms@gmail.com>
2023-11-10 17:15:43 -0300

    feat: add SubscribeModal component to ModalProvider

---
+++ components/SubscribeModal.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Modal from "./Modal";
+
+const SubsribeModal = () => {
+  return (
+    <Modal
+      title="Only for premium users"
+      description="Listen to music with SoundVoyage Premium"
+      isOpen
+      onChange={() => {}}
+    >
+      Subscription
+    </Modal>
+  );
+};
+
+export default SubsribeModal;
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthModal from "@/components/AuthModal";
+import SubsribeModal from "@/components/SubscribeModal";
 import UploadModal from "@/components/UploadModal";
 import { useEffect, useState } from "react";
 
@@ -17,6 +18,7 @@ const ModalProvider = () => {
     <>
       <AuthModal />
       <UploadModal />
+      <SubsribeModal />
     </>
   );
 };
