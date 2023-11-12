6919345c36735259ca334997dc3569101f93531c
Fellipe Marcel <fmrclms@gmail.com>
2023-11-12 09:26:00 -0300

    chore: remove comments

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -2,12 +2,11 @@ import { Price } from "@/types";
 
 export const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.NEXT_PUBLIC_SITE_URL ??
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
     "http://localhost:3000/";
-  // Make sure to include `https://` when not localhost.
+
   url = url.includes("http") ? url : `https://${url}`;
-  // Make sure to including trailing `/`.
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
   return url;
 };
@@ -38,7 +37,7 @@ export const postData = async ({
 };
 
 export const toDateTime = (secs: number) => {
-  var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start.
+  var t = new Date("1970-01-01T00:30:00Z");
   t.setSeconds(secs);
   return t;
 };
