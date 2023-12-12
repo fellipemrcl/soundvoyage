dad8b97dcd5e9ef9a9cdcb4bf435209b8daa2727
Fellipe Marcel <fmrclms@gmail.com>
2023-12-12 13:47:18 -0300

    feat: adds dynamic greeting based on system time

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,11 +8,33 @@ export const revalidate = 0;
 export default async function Home() {
   const songs = await getSongs();
 
+  const getSystemTime = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  const returnGreetingAccordingToTime = () => {
+    const time = getSystemTime();
+    const hour = time.split(":")[0];
+
+    if (Number(hour) >= 0 && Number(hour) < 12) {
+      return "Good morning!";
+    } else if (Number(hour) >= 12 && Number(hour) < 18) {
+      return "Good afternoon!";
+    } else {
+      return "Good evening!";
+    }
+  }
+
   return (
     <div className="w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-950">
       <Header>
         <div className="mb-2">
-          <h1 className="text-3xl font-semibold text-white">Welcome back</h1>
+          <h1 className="text-3xl font-semibold text-white">{returnGreetingAccordingToTime()}</h1>
           <div className="grid grid-cols-1 gap-3 mt-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
             <ListItem
               image="/images/liked.png"
